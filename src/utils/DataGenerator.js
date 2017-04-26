e79dad2cd552f66cb79555d022ced1fe93e9bed1
Rodrigo Roa Rodríguez <rodrigoroarod@gmail.com>
2017-04-26 14:48:32 +0200

    feat: function to get individual distributions

--- src/utils/DataGenerator.js
+++ src/utils/DataGenerator.js
@@ -4,8 +4,8 @@ const { sqrt, PI, exp, log } = Math;
 function range(count) { return Array(count).fill().map((_, i) => i); }
 
 export function getDistro(distro, { count, ...rest }) {
-  const x = range(count);
-  Distributions[distro]({ x, count, ...rest });
+  const xs = range(count);
+  return xs.map(x => Distributions[distro]({ x, xs, count, ...rest }));
 }
 
 export function getDistroArray({ count = 100, ...rest } = {}) {
