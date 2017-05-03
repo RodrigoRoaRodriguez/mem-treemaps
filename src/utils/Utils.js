8c9c2d8bb43e5a1b645c623021b65aeb41ce20be
Rodrigo Roa Rodríguez <rodrigoroarod@gmail.com>
2017-05-03 12:13:22 +0200

    chore: update utils.

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,5 +1,5 @@
 export function range(count) {
-  return Array(count).fill().map((_, i) => i); 
+  return Array(count).fill().map((_, i) => i);
 }
 
 export function sum(array) {
@@ -8,5 +8,11 @@ export function sum(array) {
 
 export function elementwise(fn, ...args) {
   return args[0].map((_, i, arrays) => fn(arrays.map(() => arrays[i])));
+}
+
+export function map2d(arrayOfArrays, fn) {
+  return arrayOfArrays.map((outerValue, outerIndex, outerArray) =>
+    outerValue.map((value, index, array) =>
+      fn(value, index, array, outerIndex, outerArray)));
 }
 
